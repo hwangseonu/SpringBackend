2abcdb7dbb679c92220cedb7cc1807026f8ad396
hwangseonu <hwangseonu12@naver.com>
2019-02-20 17:05:07 +0900

    for rebuild

--- src/main/java/me/mocha/backend/common/model/repository/UserRespository.java
+++
@@ -1,7 +0,0 @@
-package me.mocha.backend.common.model.repository;
-
-import me.mocha.backend.common.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRespository extends JpaRepository<User, String> {
-}
