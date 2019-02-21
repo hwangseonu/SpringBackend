2c2b061d296329f288c42134bb38092027271543
hwangseonu <hwangseonu12@naver.com>
2019-02-21 16:11:52 +0900

    fix: User model에 필요한 필드들 정의

--- src/main/java/me/mocha/backend/common/model/entity/User.java
+++ src/main/java/me/mocha/backend/common/model/entity/User.java
@@ -7,9 +7,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -21,10 +26,21 @@ public class User implements UserDetails {
     @Id
     private String username;
 
+    @NotNull
     private String password;
 
-    transient private Collection<GrantedAuthority> authorities;
+    @NotNull
+    private String nickname;
 
+    @NotNull
+    @Email
+    private String email;
+
+    @NotEmpty
+    @ElementCollection(targetClass = String.class)
+    private List<String> roles;
+
+    transient private Collection<GrantedAuthority> authorities;
 
     @Override
     public boolean isAccountNonExpired() {
