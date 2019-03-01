ec9a6e6353d314e1b99a745610f4a8060cc32861
hwangseonu <hwangseonu12@naver.com>
2019-03-01 20:13:36 +0900

    fix: SpringBootTest를 WebMvcTest로 바꿈

--- src/test/java/me/mocha/backend/AuthControllerTest.java
+++ src/test/java/me/mocha/backend/AuthControllerTest.java
@@ -3,27 +3,30 @@ package me.mocha.backend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.mocha.backend.common.model.entity.User;
 import me.mocha.backend.common.model.repository.UserRepository;
+import me.mocha.backend.common.security.UserDetailsServiceImpl;
 import me.mocha.backend.common.security.jwt.JwtProvider;
+import me.mocha.backend.common.security.jwt.JwtType;
+import me.mocha.backend.user.controller.AuthController;
 import me.mocha.backend.user.request.SignInRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
+import java.util.Optional;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = {AuthController.class}, secure = false)
 public class AuthControllerTest {
 
     @Autowired
@@ -32,14 +35,39 @@ public class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
+    @MockBean
     private UserRepository userRepository;
 
-    @Autowired
+    @MockBean
+    private JwtProvider jwtProvider;
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockBean
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private JwtProvider jwtProvider;
+    @Test
+    public void signIn_success() throws Exception {
+        given(userRepository.findById("test1234")).willReturn(Optional.of(User.builder()
+                .username("test1234")
+                .password("test1234")
+                .nickname("test1234")
+                .email("test@email.com")
+                .build()));
+        given(jwtProvider.createToken("test1234", JwtType.ACCESS)).willReturn("access");
+        given(jwtProvider.createToken("test1234", JwtType.REFRESH)).willReturn("refresh");
+        given(passwordEncoder.matches("test1234", "test1234")).willReturn(true);
+
+        SignInRequest request = new SignInRequest("test1234", "test1234");
+        mockMvc.perform(post("/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access").value("access"))
+                .andExpect(jsonPath("$.refresh").value("refresh"));
+    }
 
     @Test
     public void signIn_notfound() throws Exception {
@@ -52,22 +80,20 @@ public class AuthControllerTest {
     }
 
     @Test
-    public void signIn_success() throws Exception {
-        SignInRequest request = new SignInRequest("test1234", "test1234");
-        userRepository.save(User.builder()
+    public void signIn_unauthorized() throws Exception {
+        given(userRepository.findById("test1234")).willReturn(Optional.of(User.builder()
                 .username("test1234")
-                .password(passwordEncoder.encode("test1234"))
+                .password("test1234")
                 .nickname("test1234")
-                .email("test1234")
-                .roles(Collections.singletonList("ROLE_USER"))
-                .build());
+                .email("test@email.com")
+                .build()));
+        given(passwordEncoder.matches("test1234", "test1111")).willReturn(false);
+        SignInRequest request = new SignInRequest("test1234", "test1111");
         mockMvc.perform(post("/auth")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.access").isNotEmpty())
-                .andExpect(jsonPath("$.refresh").isNotEmpty());
+                .andExpect(status().isUnauthorized());
     }
 
 }
