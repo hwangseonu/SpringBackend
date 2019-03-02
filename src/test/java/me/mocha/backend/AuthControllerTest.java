bcf9a8c625c06b70e774bbe2daae6b6f517eb36d
hwangseonu <hwangseonu12@naver.com>
2019-03-03 01:14:08 +0900

    fix: JwtAuthenticationFilter를 SpyBean으로 선언

--- src/test/java/me/mocha/backend/AuthControllerTest.java
+++ src/test/java/me/mocha/backend/AuthControllerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import me.mocha.backend.common.model.entity.User;
 import me.mocha.backend.common.model.repository.UserRepository;
 import me.mocha.backend.common.security.UserDetailsServiceImpl;
+import me.mocha.backend.common.security.jwt.JwtAuthenticationEntryPoint;
+import me.mocha.backend.common.security.jwt.JwtAuthenticationFilter;
 import me.mocha.backend.common.security.jwt.JwtProvider;
 import me.mocha.backend.common.security.jwt.JwtType;
 import me.mocha.backend.user.controller.AuthController;
@@ -13,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,7 +24,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = {AuthController.class}, secure = false)
+@WebMvcTest(value = {AuthController.class})
 public class AuthControllerTest {
 
     @Autowired
@@ -38,38 +43,43 @@ public class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
-    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @MockBean
     private JwtProvider jwtProvider;
 
     @MockBean
+    private UserRepository userRepository;
+
+    @SpyBean
     private UserDetailsServiceImpl userDetailsService;
 
-    @MockBean
-    private PasswordEncoder passwordEncoder;
+    @SpyBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @SpyBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
     public void signIn_success() throws Exception {
-        given(userRepository.findById("test1234")).willReturn(Optional.of(User.builder()
-                .username("test1234")
-                .password("test1234")
-                .nickname("test1234")
-                .email("test@email.com")
-                .build()));
-        given(jwtProvider.createToken("test1234", JwtType.ACCESS)).willReturn("access");
-        given(jwtProvider.createToken("test1234", JwtType.REFRESH)).willReturn("refresh");
-        given(passwordEncoder.matches("test1234", "test1234")).willReturn(true);
+        String username = "test1234";
+        String email = "test@email.com";
+        String access = "access";
+        String refresh = "refresh";
+        User user = User.builder().username(username).password(passwordEncoder.encode(username)).nickname(username).email(email).build();
+        given(userRepository.findById(username)).willReturn(Optional.of(user));
+        given(jwtProvider.createToken(username, JwtType.ACCESS)).willReturn(access);
+        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn(refresh);
 
-        SignInRequest request = new SignInRequest("test1234", "test1234");
+        SignInRequest request = new SignInRequest(username, username);
         mockMvc.perform(post("/auth")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.refresh").value("refresh"))
-                .andExpect(jsonPath("$.access").value("access"))
+                .andExpect(jsonPath("$.access").value(access))
+                .andExpect(jsonPath("$.refresh").value(refresh))
                 .andDo(print());
     }
 
@@ -86,19 +96,54 @@ public class AuthControllerTest {
 
     @Test
     public void signIn_unauthorized() throws Exception {
-        given(userRepository.findById("test1234")).willReturn(Optional.of(User.builder()
-                .username("test1234")
-                .password("test1234")
-                .nickname("test1234")
-                .email("test@email.com")
-                .build()));
-        given(passwordEncoder.matches("test1234", "test1111")).willReturn(false);
-        SignInRequest request = new SignInRequest("test1234", "test1111");
+        String username = "test1234";
+        String email = "test@email.com";
+        String access = "access";
+        String refresh = "refresh";
+        User user = User.builder().username(username).password(passwordEncoder.encode(username)).nickname(username).email(email).build();
+        given(userRepository.findById(username)).willReturn(Optional.of(user));
+        given(jwtProvider.createToken(username, JwtType.ACCESS)).willReturn(access);
+        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn(refresh);
+
+        SignInRequest request = new SignInRequest(username, "test1111");
         mockMvc.perform(post("/auth")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @Test
+    public void signIn_badRequest() throws Exception {
+        SignInRequest request = new SignInRequest("a", "a");
+        mockMvc.perform(post("/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    public void refresh_success() throws Exception {
+        String access = "access";
+        String refresh = "refresh";
+        String username = "username";
+
+        given(jwtProvider.createToken(username, JwtType.ACCESS)).willReturn(access);
+        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn(refresh);
+        given(jwtProvider.isValid(refresh, JwtType.REFRESH)).willReturn(true);
+        given(jwtProvider.getExpiration(refresh)).willReturn(LocalDateTime.now().plusDays(6));
+        when(jwtProvider.getUsername(any())).thenReturn(username);
+        given(userRepository.existsById(username)).willReturn(true);
+
+        mockMvc.perform(get("/auth/refresh")
+                .header("Authorization", "Bearer " + refresh)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access").value(access))
+                .andExpect(jsonPath("$.refresh").value(refresh))
                 .andDo(print());
     }
 
@@ -112,10 +157,20 @@ public class AuthControllerTest {
 
     @Test
     public void refresh_unprocessableEntity() throws Exception {
-        String jwt = "access";
-        given(jwtProvider.isValid(jwt, JwtType.REFRESH)).willReturn(false);
+        String refresh = "refresh";
+        String username = "username";
+
+        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn(refresh);
+        given(jwtProvider.isValid(refresh, JwtType.REFRESH)).willReturn(false);
+
         mockMvc.perform(get("/auth/refresh")
-                .header("Authorization", "Bearer " + jwt)
+                .header("Authorization", "Bearer " + refresh)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+
+        mockMvc.perform(get("/auth/refresh")
+                .header("Authorization", "Bearer ")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnprocessableEntity())
                 .andDo(print());
@@ -123,37 +178,18 @@ public class AuthControllerTest {
 
     @Test
     public void refresh_notfound() throws Exception {
-        String jwt = "refresh";
-        String username = "test1234";
-        given(jwtProvider.isValid(jwt, JwtType.REFRESH)).willReturn(true);
-        given(jwtProvider.getUsername(jwt)).willReturn(username);
+        String refresh = "refresh";
+        String username = "username";
+
+        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn(refresh);
+        given(jwtProvider.isValid(refresh, JwtType.REFRESH)).willReturn(true);
+        when(jwtProvider.getUsername(any())).thenReturn(username);
         given(userRepository.existsById(username)).willReturn(false);
 
         mockMvc.perform(get("/auth/refresh")
-                .header("Authorization", "Bearer " + jwt)
+                .header("Authorization", "Bearer " + refresh)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
-                .andDo(print());
-    }
-
-    @Test
-    public void refresh_success() throws Exception {
-        String jwt = "refresh";
-        String username = "test1234";
-
-        given(jwtProvider.isValid(jwt, JwtType.REFRESH)).willReturn(true);
-        given(jwtProvider.getUsername(jwt)).willReturn(username);
-        given(jwtProvider.createToken(username, JwtType.ACCESS)).willReturn("access");
-        given(jwtProvider.createToken(username, JwtType.REFRESH)).willReturn("refresh");
-        given(jwtProvider.getExpiration(jwt)).willReturn(LocalDateTime.now().plusDays(6));
-        given(userRepository.existsById(username)).willReturn(true);
-
-        mockMvc.perform(get("/auth/refresh")
-                .header("Authorization", "Bearer " + jwt)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.access").value("access"))
-                .andExpect(jsonPath("$.refresh").value("refresh"))
                 .andDo(print());
     }
 
