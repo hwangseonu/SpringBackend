a8dbd6994ac1603dceef6de7ae95576bc4c4ab63
hwangseonu <hwangseonu12@naver.com>
2019-03-03 01:36:05 +0900

    feat: Add PostControllerTest

--- src/test/java/me/mocha/backend/PostControllerTest.java
+++ src/test/java/me/mocha/backend/PostControllerTest.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import me.mocha.backend.common.model.entity.User;
 import me.mocha.backend.common.model.repository.UserRepository;
 import me.mocha.backend.common.security.UserDetailsServiceImpl;
+import me.mocha.backend.common.security.jwt.JwtAuthenticationEntryPoint;
+import me.mocha.backend.common.security.jwt.JwtAuthenticationFilter;
 import me.mocha.backend.common.security.jwt.JwtProvider;
 import me.mocha.backend.post.controller.PostController;
 import me.mocha.backend.post.model.entity.Post;
 import me.mocha.backend.post.model.repository.PostRepository;
 import me.mocha.backend.post.request.NewPostRequest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -32,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = {PostController.class}, secure = false)
+@WebMvcTest({PostController.class})
 public class PostControllerTest {
 
     @Autowired
@@ -50,18 +54,30 @@ public class PostControllerTest {
     @MockBean
     private JwtProvider jwtProvider;
 
-    @MockBean
+    @SpyBean
     private UserDetailsServiceImpl userDetailsService;
 
-    @WithMockUser(username = "test1234")
+    @SpyBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @SpyBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    final private String username = "test1234";
+    private User user = User.builder().username(username).password(username).nickname(username).email("test@email.com").roles(Collections.singletonList("ROLE_USER")).build();
+
+    @Before
+    public void setUp() {
+        given(userRepository.findById(username)).willReturn(Optional.of(user));
+    }
+
+    @WithMockUser(username = username)
     @Test
     public void createPost_success() throws Exception {
         String title = "test";
         String content = "test";
         Date date = new Date();
-        User user = User.builder().username("test1234").password("test1234").nickname("test1234").email("test@email.com").roles(Collections.singletonList("ROLE_USER")).build();
         Post post = Post.builder().id(1).title(title).content(content).createAt(date).updateAt(date).views(0).writer(user).build();
-        given(userRepository.findById("test1234")).willReturn(Optional.of(user));
         when(postRepository.save(any(Post.class))).thenReturn(post);
         NewPostRequest request = new NewPostRequest(title, content);
         mockMvc.perform(post("/posts")
@@ -71,6 +87,12 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.title").value(title))
                 .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.writer.username").value(username))
+                .andExpect(jsonPath("$.writer.nickname").value(username))
+                .andExpect(jsonPath("$.writer.email").value("test@email.com"))
+                .andExpect(jsonPath("$.createAt").value(date))
+                .andExpect(jsonPath("$.updateAt").value(date))
+                .andExpect(jsonPath("$.views").value(0))
                 .andDo(print());
     }
 
